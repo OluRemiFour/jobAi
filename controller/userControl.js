ec39ce7bb4f2644f79078ef3b1ac054004b321bc
OluRemiFour <lekanayoola04@gmail.com>
2025-05-08 17:46:20 +0100

    updates

--- controller/userControl.js
+++ controller/userControl.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 exports.allUsers = async (req, res) => {
@@ -12,7 +13,24 @@ exports.allUsers = async (req, res) => {
 
 exports.getUser = async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
+    // const user = await User.findById(req.params.id);
+    let token;
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (!authHeader) {
+      return res.status(404).json({ message: "Authorization not found" });
+    }
+
+    if (authHeader.startsWith("Bearer")) {
+      token = authHeader.split(" ")[1];
+    } else {
+      return res.status(401).json({
+        message: "Invalid authorization format",
+      });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRECT);
+
+    const user = await User.findById(decodedToken.id);
     if (!user) {
       return res.status(404).json({
         message: "User not found",
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ const route = express.Router();
 const { allUsers, getUser } = require("../controller/userControl");
 
 route.get("/", allUsers);
-route.get("/:id", getUser);
+// route.get("/:id", getUser);
+route.get("/getUser", getUser);
 
 module.exports = route;
