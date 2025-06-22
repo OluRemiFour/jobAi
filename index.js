85a521a83624d48e1e4bc4eca0bf5ce2cb4d6ca2
OluRemiFour <lekanayoola04@gmail.com>
2025-06-22 05:20:08 -0700

    dir mail

--- index.js
+++ index.js
@@ -81,7 +81,8 @@ app.all(/(.*)/, (req, res, next) => {
 
 mongoose
   .connect(process.env.MONGODB_URL, {
-    
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
